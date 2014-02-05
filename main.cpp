f3b1c15f9e7864fa27fb345892dad44e884616ff
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2014-02-05 14:35:27 +0200

    Added Examples and simple CMakeLists

--- main.cpp
+++ main.cpp
@@ -7,33 +7,37 @@ using namespace std;
 
 int main()
 {
-	/*TArray<int> testArray;
+	cout<<"TArray example: \n";
+	TArray<int> testArray;
 	testArray.push_back(10);
 	testArray.push_back(20);
 	testArray.push_front(1);
 	testArray.Clear();
 	for(TArray<int>::iterator it=testArray.begin();it!=testArray.end();it++)
-		cout<<*it<<endl;*/
+		cout<<*it<<endl;
 
-	/*TBinaryTree<int> tree;
+	cout<<"TBinaryTree example: \n";
+	TBinaryTree<int> tree;
 	tree.addElement(20);
 	tree.addElement(10);
 	tree.addElement(40);
 	tree.addElement(2);
 	tree.addElement(12);
 	tree.remove(10);
-	tree.displayPreOrder();*/
+	tree.displayPreOrder();
 
-	/*TDeque<int> deque;
+	cout<<"TDeque example: \n";
+	TDeque<int> deque;
 	deque.push_back(10);
 	deque.push_front(20);
 	deque.push_front(30);
 	deque.push_back(3);
 	for(TDeque<int>::iterator it=deque.begin();it!=deque.end();it++)
 		cout<<*it<<endl;
+	cout<<"Show items without iterators:\n";
 	for(int i=0;i<deque.Size();i++)
-		cout<<deque[i]<<endl;*/
+		cout<<deque[i]<<endl;
 
-	TLinkedList<int> list;
+	TList<int> list;
 	return 0;
 }
